f321bd4afdac141a570d3347a39c84a7a22439fe
Leonore Guillain <leonore.guillain@epfl.ch>
2017-11-28 08:38:04 +0100

    Adding some text, adding run.py

---
+++ project/run.py
@@ -0,0 +1,40 @@
+from pyspark.sql import *
+from pyspark import SparkContext, SQLContext
+from pyspark.sql.functions import regexp_replace, col, lower, explode
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
+df = spark.read.json('swiss-tweet/')
+
+#remove nesting
+df = df.select('_source.*')
+
+#columns we care about
+columns = ['main', 'published', 'source_spam_probability', 'source_location', 'tags', 'lang', 'sentiment',
+                   'author_gender', 'source_followers', 'source_following']
+
+#get only relevant columns
+df = df[columns]
+
+df = df.filter(df.lang.isin('en', 'de', 'fr'))  #gets tweets with right language
+df = df.filter(df.sentiment != 'POSITIVE') #remove positive sentient tweets
+df = df.filter(df.source_spam_probability < 0.5) #remove spam
+df = df.withColumn('main', lower(df.main)) #get lower case
+
+df = df.withColumn('main', regexp_replace(col('main'), 'pic.twitter\S+', ' '))#remove picture urls
+df = df.withColumn('main', regexp_replace(col('main'), '@\S+', ' '))#remove mentions
+
+#remove websites and retweets
+df = df.filter(~(df.main.contains('http')))\
+    .filter(~(df.main.contains('.com')))\
+    .filter(~(df.main.contains('.ch')))\
+    .filter(~(df.main.contains('www')))\
+    .filter(~(df.main.contains('RT')))
+
+
+df.write.json('reduced_tweets') #write dataframe to folder in json format
